8b4b0c8b68f1fbc7477c83583d3db94ef33b1784
Ayush Pal <ayushpal15326@gmail.com>
2024-10-05 17:26:19 +0530

    adding Props

--- reactapp/src/components/Netflixcard.jsx
+++ reactapp/src/components/Netflixcard.jsx
@@ -1,33 +1,13 @@
 import seriesData from "../api/seriesData.json";
-// name export components using export keyword
+import { SeriesCard } from "./SeriesCard";
 export const Netflixcard = () => {
-  // Dynamic Values creating
-  // const name = "Queen of Tears"
-  // const age = 16
   return (
     <ul>
-      {seriesData.map((curElem) => {
-        return (
-          <li>
-            <div>
-              <img src={curElem.img_url} width="40%" height="40%" alt="" />
-            </div>
-            <h2>Name: {curElem.name}</h2>
-            <h3>Rating: {curElem.rating}</h3>
-            <p>Summary: {curElem.description}</p>
-            <p>Genre: {curElem.genre}</p>
-            <p>Cast: {curElem.cast}</p>
-            <a href={curElem.watch_url} target="_blank">
-              <button>Watch Now</button>
-            </a>
-            {/* Ternary Condition Using
-          <button> {age >= 18 ? "Watch Now" : "Not Available"}</button> */}
-          </li>
-        );
-      })}
+      {seriesData.map((curElem) => ( <SeriesCard key={curElem.id} curElem={curElem} />
+      ))}
     </ul>
   );
 };
 
 // exporting default components
-// export default Netflixcard
+export default Netflixcard
