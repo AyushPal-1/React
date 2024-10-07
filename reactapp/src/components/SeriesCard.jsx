b4ab850622084dc7d0fb914353452f10f1cffb84
Ayush Pal <ayushpal15326@gmail.com>
2024-10-07 16:32:42 +0530

    Adding styled components and tailwindcss

--- reactapp/src/components/SeriesCard.jsx
+++ reactapp/src/components/SeriesCard.jsx
@@ -1,15 +1,21 @@
 import styles from "./Netflix.module.css";
+import styled from "styled-components";
 export const SeriesCard = (props) => {
   const { id, img_url, name, rating, description, cast, genre, watch_url } =
     props.curElem;
 
-  const btn_style = {
+  // const btn_style = {}
+  // Styling a Button with Styled Components: Style Objects
+    const Buttonwatch = styled.button ({
     padding: "1.2rem 2.4rem",
     border: "none",
     fontSize: "1.6rem",
     backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
-    color: "var(--bg-color)"
-  };
+    color: "var(--bg-color)",
+    fontWeight: "bold",
+    cursor: "pointer",
+  });
+
   const ratingClass = rating >= 8.5 ? styles.super_hit : styles.average
 
   return (
@@ -22,11 +28,12 @@ export const SeriesCard = (props) => {
         <h3>
           Rating: <span className={`${styles.rating} ${ratingClass}`}>{rating}</span>
         </h3>
-        <p>Summary: {description}</p>
+        <p className="text-3xl font-bold underline">Summary: {description}</p>
         <p>Genre: {genre}</p>
         <p>Cast: {cast}</p>
         <a href={watch_url} target="_blank">
-          <button style={btn_style}>Watch Now</button>
+          {/* <button style={btn_style}>Watch Now</button> */}
+          <Buttonwatch>Watch Now</Buttonwatch>
         </a>
         {/* Ternary Condition Using
         <button> {age >= 18 ? "Watch Now" : "Not Available"}</button> */}
